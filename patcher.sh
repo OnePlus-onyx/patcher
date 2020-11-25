89edf7593e2715b5fe3035423e3d737b6a06892a
ittat <ittat@live.com>
2020-11-25 08:50:29 +0800

    Update patcher.sh

--- patcher.sh
+++ patcher.sh
@@ -18,18 +18,26 @@ REPOSITORIES=(
 
 for repository in "${REPOSITORIES[@]}"; do
     cd "${ROOT}/${repository}"
-
-    git am --keep-cr "${ROOT}/patcher/patches/${repository}"/*
-
+        for patch in ${ROOT}/patcher/patches/${repository}*.patch; do
+                #Check if patch is already applied
+                if patch -f -p1 --dry-run -R < $patch > /dev/null;then
+                        echo -e "\e[1;33m [Skip] \e[0m  ${patch##*/}"
+                        continue
+                fi
+                #Ckeck patch error
+                git apply --check  $patch  2> ${ROOT}/patcher/patch.tmp
+                if [ -s ${ROOT}/patcher/patch.tmp ]; then
+                    echo -e "\e[1;31m [Error] \e[0m removed: ${patch##*/}"
+                    rm ${ROOT}/patcher/patch.tmp
+                else
+                    echo -e "\e[1;32m [Patch] \e[0m  ${patch##*/}"
+                    git am $patch > /dev/null
+                fi
+        done 
     cd "${ROOT}"
 done
 
 
-#echo "Switch libcarthage to https://github.com/OnePlus-onyx/libcarthage" 
-#cd ${ROOT}/gonk-misc
-#rm -rf libcarthage
-#git clone https://github.com/OnePlus-onyx/libcarthage -b onyx --depth=1
-#echo "Done!" 
 
 cd ${ROOT}
 
