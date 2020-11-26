cf56014ba579c6bb29625733fcee9aa3b54025e0
ittat <ittat@live.com>
2020-11-26 13:24:36 +0800

    Update patcher.sh

--- patcher.sh
+++ patcher.sh
@@ -18,18 +18,38 @@ REPOSITORIES=(
 
 for repository in "${REPOSITORIES[@]}"; do
     cd "${ROOT}/${repository}"
-
-    git am --keep-cr "${ROOT}/patcher/patches/${repository}"/*
-
+        for patch in ${ROOT}/patcher/patches/${repository}/*.patch; do
+                #Check if patch is already applied
+                if patch -f -p1 --dry-run -R < $patch > /dev/null;then
+                        echo "[Skip] patch is already applied? ${patch##*/}"
+                        echo "[log]: "
+                        patch -f -p1 --dry-run -R < $patch > /dev/stdout
+                        continue
+                fi
+                #Ckeck patch error
+                git apply --check  $patch  2> ${ROOT}/patcher/patch.tmp
+                if [ -s ${ROOT}/patcher/patch.tmp ]; then
+                    echo "[Warm] ${patch##*/} maybe can't patch"
+                    echo "[Warm] force to apply"
+                    rm ${ROOT}/patcher/patch.tmp
+                    git am $patch 1> /dev/null  2> /dev/null
+                    if patch -f -p1 --dry-run -R < $patch > /dev/null;then
+                            echo "[Succeed] ${patch##*/}"
+                    else
+                        echo "[Error!] failed! abort"
+                        echo "[log]: "
+                        patch -f -p1 --dry-run -R < $patch > /dev/stdout
+                        git am --abort
+                    fi
+                else
+                    echo  "[Patch] ${patch##*/}"
+                    git am $patch > /dev/null
+                fi
+        done 
     cd "${ROOT}"
 done
 
-
-#echo "Switch libcarthage to https://github.com/OnePlus-onyx/libcarthage" 
-#cd ${ROOT}/gonk-misc
-#rm -rf libcarthage
-#git clone https://github.com/OnePlus-onyx/libcarthage -b onyx --depth=1
-#echo "Done!" 
+echo "Done. Each patch is essence. If find patch failed, do \"repo sync\" rollback HEAD"
 
 cd ${ROOT}
 
